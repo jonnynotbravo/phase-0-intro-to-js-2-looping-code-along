d70ff2ae3fa90d3ef2b78b57fe606f243dd246f2
Jonny Tilahun <Jonnybiruk13@gmail.com>
2022-09-23 22:35:08 -0400

    commit tree 2e3ebe434505cb838aac21fe846089723efbd7fe

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solutions in this file
+function writeCards(arr, event){
+
+    let newArr = [];
+
+    for(let i = 0; i < arr.length; i++){
+
+        
+
+        newArr.push(`Thank you, ${arr[i]}, for the wonderful ${event} gift!`);
+
+    }
+    
+    return newArr;
+
+
+}
+
+function countDown(num){
+
+    let i = num;
+    
+    while(i >= 0){
+
+        console.log(i);
+        i--;
+
+    }
+}
